c9a4d08c9b93763530ab2df8893dd2d944cf1108
chintan90fullstack <chintantrivedi90@gmail.com>
2025-04-02 16:46:55 +0530

    Commit for E1E2E3

---
+++ README.md
@@ -0,0 +1,20 @@
+# Namaste React first app
+
+
+# parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differntial Bundling - Support older browser
+- Diagnostic
+- Error Handling
+- Https Support
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
--- app.js
+++ app.js
@@ -1,43 +1,46 @@
-const heading1 = React.createElement(
-    "h1",
-    {
-        class:"heading1", 
-    }, 
-    "I am the h1"
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const heading2 = React.createElement(
-    "h2",
-    {
-        class:"heading2", 
-    }, 
-    "I am the h2"
-);
 
-const child1 = React.createElement(
-    "div",
-    {
-        id:"child1", 
-    }, 
-    [heading1, heading2]
-);
+// React Element
+/*
+const heading = React.createElement("h1", { id: "heading"}, "Namaste React.");
+console.log(heading);*/
 
-const child2 = React.createElement(
-    "div",
-    {
-        id:"child2", 
-    }, 
-    [heading1, heading2]
-);
+// JSX written element
+const jsxHeading = (<h1 id="heading" className="testclass" tabIndex="1">Namaste React using JSX.</h1>);
+// console.log(jsxHeading);
 
-const parent = React.createElement(
-    "div",
-    {
-        id: "parent",
-    },
-    [child1, child2]
-);
+// React Functional Component
+// const Title = () => (<h1>This is Another Title.</h1>)
+// const Title = () => { return <h1>This is Another Title.</h1>}
+// const Title = () => <h1>This is Another Title.</h1>
+// const Title = function() { return (<h1>This is Another Title.</h1>) } // not widely used
 
+// above all are different ways to show a functional component
+const num = 32;
+const Title = () => (
+    <div className="parentTitle">
+        <h1>This is Another Title.</h1>
+        <h2>My age is: {num}</h2>
+        
+    </div>
+)
+
+const NewComp = () => ( <div>Thiw will goes under another component</div>)
+
+
+// This is Component Composition
+const HeadingComponent1 = () => (
+<div>
+    {Title()}
+    <Title></Title>
+    <Title/>
+    <NewComp/>
+<h1>This is React Functional Component</h1>
+</div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(jsxHeading);
+root.render(<HeadingComponent1/>);
