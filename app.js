86c950f0be24c4ecf2c405bb24528d1cfb87d8c4
chintan90fullstack <chintantrivedi90@gmail.com>
2025-04-03 17:24:21 +0530

    Passing arguments in functional components

--- app.js
+++ app.js
@@ -2,45 +2,95 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-// React Element
+
 /*
-const heading = React.createElement("h1", { id: "heading"}, "Namaste React.");
-console.log(heading);*/
+* Header
+*  - Logo
+*  - Nav Items
+*
+* Body
+*  - Search
+*  - Restaurant Container
+*    - RestaurantCard
+*       - image
+*       - Name
+*       - star rating
+*       - cusisine
+*       - delivery time
+* Footer
+*  - Copyright
+*  - Links
+*  - Address
+*  - Contact
+ */
 
-// JSX written element
-const jsxHeading = (<h1 id="heading" className="testclass" tabIndex="1">Namaste React using JSX.</h1>);
-// console.log(jsxHeading);
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="/images/FoodLogo.png" alt="Logo Image"/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+};
 
-// React Functional Component
-// const Title = () => (<h1>This is Another Title.</h1>)
-// const Title = () => { return <h1>This is Another Title.</h1>}
-// const Title = () => <h1>This is Another Title.</h1>
-// const Title = function() { return (<h1>This is Another Title.</h1>) } // not widely used
+// either receive variables in a single object and show them after picking them from object like props.resName OR simply take all variables as parapeters.
 
-// above all are different ways to show a functional component
-const num = 32;
-const Title = () => (
-    <div className="parentTitle">
-        <h1>This is Another Title.</h1>
-        <h2>My age is: {num}</h2>
-        
-    </div>
-)
+const RestuarntCard = (props) => {
+    // const RestuarntCard = ({resImage, resName, resCuisine, resRating, resDeliveryTime}) => {
+    const {resImage, resName, resCuisine, resRating, resDeliveryTime} = props;
+    return (
+        <div className="res-card">
+            <img className="res-logo" src={resImage} alt={resName} />
+            <h3>{props.resName}</h3>
+            <h4>{resCuisine}</h4>
+            <h4>{resRating}</h4>
+            <h4>{resDeliveryTime}</h4>
+        </div>
+    )
+};
 
-const NewComp = () => ( <div>Thiw will goes under another component</div>)
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">
+                Search box
+            </div>
+            <div className="restro_container">
+                <RestuarntCard resImage="https://img-getpocket.cdn.mozilla.net/404x202/filters:format(jpeg):quality(60):no_upscale():strip_exif()/https%3A%2F%2Fs3.us-east-1.amazonaws.com%2Fpocket-curatedcorpusapi-prod-images%2Fc3ab63df-5921-4b0a-bfed-ebe9027047b7.jpeg" resName="Meghna Foods" resCuisine="Biryani, North Indian, Chiniese, Asian" resRating="4.4 stars" resDeliveryTime="38 minutes"></RestuarntCard>
 
+                <RestuarntCard resImage="https://img-getpocket.cdn.mozilla.net/404x202/filters:format(jpeg):quality(60):no_upscale():strip_exif()/https%3A%2F%2Fs3.us-east-1.amazonaws.com%2Fpocket-curatedcorpusapi-prod-images%2Fc3ab63df-5921-4b0a-bfed-ebe9027047b7.jpeg" resName="Hotel Relaxin" resCuisine="Fast Foods" resRating="4.6 stars" resDeliveryTime="16 minutes"></RestuarntCard>
 
-// This is Component Composition
-const HeadingComponent1 = () => (
-<div>
-    {Title()}
-    <Title></Title>
-    <Title/>
-    <NewComp/>
-<h1>This is React Functional Component</h1>
-</div>
-);
+            </div>
+        </div>
+    )
+}
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            Here comes footer area
+        </div>
+    )
+};
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header></Header>
+            <Body></Body>
+            <Footer></Footer>
+        </div>
+    )
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
-root.render(<HeadingComponent1/>);
+root.render(<AppLayout/>);
